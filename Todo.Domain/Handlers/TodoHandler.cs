e65f39f77435f9948f1bfb7f67271993189abe84
Kauan <kauanhindlmayer07@gmail.com>
2022-11-24 10:29:46 -0300

    feat: add MarkTodoAsDone && MarkTodoAsUndone methods

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -7,7 +7,12 @@ using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Handlers
 {
-    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>, IHandler<UpdateTodoCommand>
+    public class TodoHandler : 
+        Notifiable, 
+        IHandler<CreateTodoCommand>, 
+        IHandler<UpdateTodoCommand>,
+        IHandler<MarkTodoAsDoneCommand>,
+        IHandler<MarkTodoAsUndoneCommand>
     {
         private readonly ITodoRepository _repository;
 
@@ -45,6 +50,46 @@ namespace Todo.Domain.Handlers
 
             // Altera o título
             todo.UpdateTitle(command.Title);
+
+            // Salva no banco
+            _repository.Create(todo);
+
+            // Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa atualizada!", todo);
+        }
+
+        public ICommandResult Handle(MarkTodoAsDoneCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
+
+            // Repera o TodoItem do banco (Reidratação)
+            var todo = _repository.GetById(command.Id, command.User);
+
+            // Altera o título
+            todo.MarkAsDone();
+
+            // Salva no banco
+            _repository.Create(todo);
+
+            // Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa atualizada!", todo);
+        }
+
+        public ICommandResult Handle(MarkTodoAsUndoneCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
+
+            // Repera o TodoItem do banco (Reidratação)
+            var todo = _repository.GetById(command.Id, command.User);
+
+            // Altera o título
+            todo.MarkAsUndone();
 
             // Salva no banco
             _repository.Create(todo);
