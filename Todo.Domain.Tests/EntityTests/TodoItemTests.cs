4c787fd214960d00d50bdd8be125abe83fa58c61
Kauan <kauanhindlmayer07@gmail.com>
2022-11-24 09:19:28 -0300

    test: add TodoItemTests

---
+++ Todo.Domain.Tests/EntityTests/TodoItemTests.cs
@@ -0,0 +1,17 @@
+﻿using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntityTests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        private readonly TodoItem _validTodo = new TodoItem("Título da tarefa", "Nome do usuário", DateTime.Now);
+
+        [TestMethod]
+        public void ShouldNotBeDoneWhenNewTodoIsCreated()
+        {
+            Assert.AreEqual(_validTodo.Done, false);
+        }
+
+    }
+}
