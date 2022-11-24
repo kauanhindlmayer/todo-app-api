0c125b446596f6db340a27dc543a7cad4b73d407
Kauan <kauanhindlmayer07@gmail.com>
2022-11-24 09:12:19 -0300

    feat: add CreateTodoHandlerTests

---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Título da tarefa", "Nome do usuário", DateTime.Now);
+
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+
+        private GenericCommandResult _result = new GenericCommandResult();
+
+        public CreateTodoHandlerTests()
+        {
+
+        }
+
+        [TestMethod]
+        public void ShouldStopRunningWhenCommandIsInvalid()
+        {
+            _result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(_result.Success, false);
+        }
+
+        [TestMethod]
+        public void ShouldCreateTodoWhenCommandIsValid()
+        {
+            _result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(_result.Success, true);
+        }
+    }
+}
