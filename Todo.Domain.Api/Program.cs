3c677388c0fc8e9f9afe6f4897915a7950d7e4e6
Kauan <kauanhindlmayer07@gmail.com>
2022-11-25 11:49:37 -0300

    fix: database provider

--- Todo.Domain.Api/Program.cs
+++ Todo.Domain.Api/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 //builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
 builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("connectionString")));
+
 builder.Services.AddTransient<ITodoRepository, TodoRepository>();
 builder.Services.AddTransient<TodoHandler, TodoHandler>();
 
--- Todo.Domain.Infra/Contexts/TodoContext.cs
+++ Todo.Domain.Infra/Contexts/TodoContext.cs
@@ -5,8 +5,10 @@ namespace Todo.Domain.Infra.Contexts
 {
     public class DataContext : DbContext
     {
+        public DataContext() { }
+
         public DataContext(DbContextOptions<DataContext> options)
-            : base (options)
+            : base(options)
         {
 
         }
